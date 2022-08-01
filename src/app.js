839eb3207840fedb1d88638f969c8e69535460d6
JeraldCrisBergantinos <44352027+JeraldCrisBergantinos@users.noreply.github.com>
2022-08-01 12:59:54 +0800

    Setup for hbs
    Added folders and files for handlebars (hbs). Added index page using handlebars.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
 
 app.set('view engine', 'hbs');
 
 app.get('/', function(req, res) {
-    res.send('<h1>Hello from the Home Page</h1>');
+    res.render('index', {
+        title: "home",
+        name: "Bergs"
+    });
 });
 
 app.get('/about', function(req, res) {
